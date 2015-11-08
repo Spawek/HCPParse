765f0a453be3680b085d5eae1f52478b7d3f5148
Spawek <spawnerer@gmail.com>
2015-11-08 14:55:57 -0800

    changed test to make it pass

--- test/Tests.hs
+++ test/Tests.hs
@@ -1,7 +1,10 @@
+
 module Main where
 
-import System.Exit (exitFailure)
+import System.Exit (exitFailure , exitSuccess)
 
 main = do
-    putStrLn "This test always fails!"
-    exitFailure
+    putStrLn "This test always pass!"
+    exitSuccess
+    -- exitFailure
+    -- putStrLn "This test always fails!"
